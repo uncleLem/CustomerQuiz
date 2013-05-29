9f71d01cee28161b1899185a7e81ea8c1601bea3
uncleLem <uncle.lem@yandex.ru>
2013-05-29 16:22:30 +0300

    removed project status and group types from admin page

--- quiz/admin.py
+++ quiz/admin.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 from quiz.models import *
 
 admin.site.register(Project)
-admin.site.register(ProjectStatus)
-admin.site.register(GroupType)
+#admin.site.register(ProjectStatus)
+#admin.site.register(GroupType)
 admin.site.register(QuestionGroup)
 admin.site.register(Question)
 admin.site.register(Answer)
